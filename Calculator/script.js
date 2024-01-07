8893808ab6416dfe01f18926b960545ade312f13
TenE18 <tchinnu5132@gmail.com>
2024-01-07 15:32:08 +0530

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+function appendToDisplay(input){
+    let display=document.getElementById("display");
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value="";
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Syntax Error";
+    }
+}
